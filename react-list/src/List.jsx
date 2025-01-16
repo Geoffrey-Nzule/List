39aa4a4c8481d3f858d6945e7e618de941466d63
Geoffrey-Nzule <geoffreynzule@gmail.com>
2025-01-16 18:13:50 +0300

    reusable components

--- react-list/src/List.jsx
+++ react-list/src/List.jsx
@@ -1,34 +1,19 @@
 
-function List() {
- // enclosing the elements with braces to make them objects
-    const fruits = [{id: 1, name: "apple", calories: 95}, 
-                    {id: 2, name: "orange", calories: 45},
-                    {id: 3, name: "banana", calories: 105},
-                    {id: 4, name: "coconut", calories: 159}, 
-                    {id: 5, name: "pineapple", calories: 37}];
+function List(props) {
 
-// Used index as the key,this prevents React from issuing warnings about missing key props when rendering list
+    const category = props.category;
 
-// if you want to sort the list you can use this function
-
-    //fruits.sort((a, b) => a.name.localeCompare(b.name));// Alphabetical order
-
-    //fruits.sort((a, b) => b.name.localeCompare(a.name));//reverse alphabetical order
-
-    //fruits.sort((a, b) => a.calories - b.calories);// sorting by calories numeric order
-    //fruits.sort((a, b) => b.calories - a.calories);// reverse numeric order
+    const itemList = props.items;
+ 
+    const listItems = itemList.map(item => <li key={item.id}>{item.name}: &nbsp; <b>{item.calories}</b></li>);
 
 
-    //const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-    const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+    return(<>
     
+    <h3>{category}</h3>
+    <ol>{listItems}</ol>;
 
-    const listItems = highCalFruits.map((highCalFruits) => <li key={highCalFruits.id}>{highCalFruits.name}: &nbsp; <b>{highCalFruits.calories}</b></li>);
-
-
-    return(<ol>{listItems}</ol>);
-
-  
+            </>);
 }
 
 export default List
