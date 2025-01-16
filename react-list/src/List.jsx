a58a2ff024153723b14a52ee568fea49addd6d7e
Geoffrey-Nzule <geoffreynzule@gmail.com>
2025-01-16 17:01:33 +0300

    Converting to objects

--- react-list/src/List.jsx
+++ react-list/src/List.jsx
@@ -1,14 +1,18 @@
 
 function List() {
- 
-    const fruits = ["apple", "orange","banana","coconut", "pineapple"];
+ // enclosing the elements with braces to make them objects
+    const fruits = [{name: "apple", calories: 95}, 
+                    {name: "orange", calories: 45},
+                    {name: "banana", calories: 105},
+                    {name: "coconut", calories: 159}, 
+                    {name: "pineapple", calories: 37}];
 
 // Used index as the key,this prevents React from issuing warnings about missing key props when rendering list
 
 // if you want to sort the list you can use this function
 
     //fruits.sort()
-    const listItems = fruits.map((fruit, index) => <li key={index}>{ fruit }</li>);
+    const listItems = fruits.map((fruit) => <li key={fruit.name}>{fruit.name}</li>);
 
 
     return(<ol>{listItems}</ol>);
